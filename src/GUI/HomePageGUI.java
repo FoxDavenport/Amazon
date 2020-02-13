233f35fef39ca7248f0c01b138ef56203314b06a
1850712 <1850712@20.8.20.32>
2020-02-13 13:11:43 -0800

    sdg

--- src/GUI/HomePageGUI.java
+++ src/GUI/HomePageGUI.java
@@ -18,73 +18,78 @@ public class HomePageGUI extends JFrame {
 	private final JButton Registry = new JButton("Registry");
 	private final JButton NewReleases = new JButton("New Releases");
 	private final JButton GiftCards = new JButton("Gift Cards");
+	private final JButton Logo= new JButton("");  
+	private final JButton VG = new JButton("");
 
 	public HomePageGUI() {
+		getContentPane().setForeground(Color.BLACK);
+		setBackground(Color.WHITE);
 		
-		getContentPane().setBackground(new Color(175, 238, 238));
+		getContentPane().setBackground(Color.WHITE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(800, 800, 800, 800);
 		getContentPane().setLayout(null);
 		
 		TodayDeals.setBorder(new MatteBorder(4, 4, 4, 4, (Color) Color.WHITE));
 		TodayDeals.setOpaque(true);
-		TodayDeals.setForeground(Color.WHITE);
-		TodayDeals.setBackground(Color.PINK);
+		TodayDeals.setForeground(Color.BLACK);
+		TodayDeals.setBackground(Color.LIGHT_GRAY);
 		TodayDeals.setBounds(224, 6, 95, 44);
 		getContentPane().add(TodayDeals);
 		
 		BestSellers.setBorder(new MatteBorder(4, 4, 4, 4, (Color) Color.WHITE));
 		BestSellers.setOpaque(true);
-		BestSellers.setForeground(Color.WHITE);
-		BestSellers.setBackground(Color.PINK);
+		BestSellers.setForeground(Color.BLACK);
+		BestSellers.setBackground(Color.LIGHT_GRAY);
 		BestSellers.setBounds(331, 62, 95, 44);
 		getContentPane().add(BestSellers);
 		
 		FindAGift.setBorder(new MatteBorder(4, 4, 4, 4, (Color) Color.WHITE));
 		FindAGift.setOpaque(true);
-		FindAGift.setForeground(Color.WHITE);
-		FindAGift.setBackground(Color.PINK);
+		FindAGift.setForeground(Color.BLACK);
+		FindAGift.setBackground(Color.LIGHT_GRAY);
 		FindAGift.setBounds(224, 62, 95, 44);
 		getContentPane().add(FindAGift);
 		
 		CustomerService.setBorder(new MatteBorder(4, 4, 4, 4, (Color) Color.WHITE));
 		CustomerService.setOpaque(true);
-		CustomerService.setForeground(Color.WHITE);
-		CustomerService.setBackground(Color.PINK);
+		CustomerService.setForeground(Color.BLACK);
+		CustomerService.setBackground(Color.LIGHT_GRAY);
 		CustomerService.setBounds(547, 6, 117, 44);
 		getContentPane().add(CustomerService);
 		
 		Registry.setBorder(new MatteBorder(4, 4, 4, 4, (Color) Color.WHITE));
 		Registry.setOpaque(true);
-		Registry.setForeground(Color.WHITE);
-		Registry.setBackground(Color.PINK);
+		Registry.setForeground(Color.BLACK);
+		Registry.setBackground(Color.LIGHT_GRAY);
 		Registry.setBounds(331, 6, 95, 44);
 		getContentPane().add(Registry);
 		
 		NewReleases.setBorder(new MatteBorder(4, 4, 4, 4, (Color) Color.WHITE));
 		NewReleases.setOpaque(true);
-		NewReleases.setForeground(Color.WHITE);
-		NewReleases.setBackground(Color.PINK);
+		NewReleases.setForeground(Color.BLACK);
+		NewReleases.setBackground(Color.LIGHT_GRAY);
 		NewReleases.setBounds(442, 6, 95, 44);
 		getContentPane().add(NewReleases);
 		
 		GiftCards.setBorder(new MatteBorder(4, 4, 4, 4, (Color) Color.WHITE));
 		GiftCards.setOpaque(true);
-		GiftCards.setForeground(Color.WHITE);
-		GiftCards.setBackground(Color.PINK);
+		GiftCards.setForeground(Color.BLACK);
+		GiftCards.setBackground(Color.LIGHT_GRAY);
 		GiftCards.setBounds(438, 62, 95, 44);
 		getContentPane().add(GiftCards);
-        
-        JButton Logo = new JButton("");        
+              
         Logo.setIcon(new ImageIcon(HomePageGUI.class.getResource("/GUI/st,small,215x235-pad,210x230,f8f8f8.u1.jpg")));
         Logo.setBounds(6, 6, 206, 103);
         getContentPane().add(Logo);
-        
-        JButton VG = new JButton("");        
+                
         VG.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
         		VideoGameGUI frame= new VideoGameGUI();
-        		frame.setVisible(true);	
+        		frame.removeAll();
+        		frame.setVG();
+        		frame.validate();
+        		frame.repaint();
         	}
         });
         
@@ -97,7 +102,34 @@ public class HomePageGUI extends JFrame {
         lblVideoGames.setBackground(Color.GREEN);
         lblVideoGames.setBounds(133, 147, 122, 16);
         getContentPane().add(lblVideoGames);
+        
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLACK);
+        panel.setBounds(0, 0, 800, 128);
+        getContentPane().add(panel);
 		
+		
+	}
+	
+	public void setHome() {
+		getContentPane().setForeground(Color.BLACK);
+		setBackground(Color.WHITE);
+		
+		getContentPane().setBackground(Color.WHITE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(800, 800, 800, 800);
+		getContentPane().setLayout(null);
+		
+		getContentPane().add(TodayDeals);
+		getContentPane().add(BestSellers);
+		getContentPane().add(FindAGift);
+		getContentPane().add(CustomerService);
+		getContentPane().add(Registry);
+		getContentPane().add(NewReleases);
+		getContentPane().add(GiftCards);
+		getContentPane().add(Logo);
+		getContentPane().add(VG);
+
 		
 	}
 }
--- src/GUI/VideoGameGUI.java
+++ src/GUI/VideoGameGUI.java
@@ -2,16 +2,16 @@ package GUI;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-
 import java.awt.*;  
 import javax.swing.*;  
-
+import java.awt.event.*;
+import javax.swing.*;
 
 public class VideoGameGUI extends JFrame{
+	
+	private final JButton FE7 = new JButton("");
+	private final JButton GBVS = new JButton("");
+	private final JButton Logo = new JButton("");
 
 	public VideoGameGUI() {
 		getContentPane().setBackground(new Color(175, 238, 238));
@@ -19,40 +19,54 @@ public class VideoGameGUI extends JFrame{
 		setBounds(800, 800, 800, 800);
 		getContentPane().setLayout(null);
 		
-		JButton Logo = new JButton("");        
 		Logo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				HomePageGUI frame= new HomePageGUI();
-        		frame.setVisible(true);
+				frame.removeAll();
+				frame.setHome();
+				frame.validate();
+				frame.repaint();
 			}
 		});
-	    Logo.setIcon(new ImageIcon(HomePageGUI.class.getResource("/GUI/st,small,215x235-pad,210x230,f8f8f8.u1.jpg")));
+	    
+		Logo.setIcon(new ImageIcon(HomePageGUI.class.getResource("/GUI/st,small,215x235-pad,210x230,f8f8f8.u1.jpg")));
 	    Logo.setBounds(6, 6, 206, 103);
 	    getContentPane().add(Logo);
 		
-		JButton FE7 = new JButton("");        
 		FE7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				FireEmblem7GUI frame= new FireEmblem7GUI();
         		frame.setVisible(true);
 			}
 		});
+		
 		FE7.setBackground(new Color(238, 238, 238));
 		FE7.setIcon(new ImageIcon(VideoGameGUI.class.getResource("/GUI/220px-GBA_Fire_Emblem_Box.jpg")));
 		FE7.setBounds(6, 121, 236, 244);
         getContentPane().add(FE7);
         
-        JButton GBVS = new JButton("");        
         GBVS.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				GBVSGUI frame= new GBVSGUI();
         		frame.setVisible(true);
 			}
 		});
+        
         GBVS.setBackground(new Color(238, 238, 238));
         GBVS.setIcon(new ImageIcon(VideoGameGUI.class.getResource("/GUI/Screen Shot 2020-02-12 at 12.28.10 PM.png")));
         GBVS.setBounds(254, 121, 236, 244);
         getContentPane().add(GBVS);
+	}
+	
+	public void setVG() {
+		getContentPane().setBackground(new Color(175, 238, 238));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(800, 800, 800, 800);
+		getContentPane().setLayout(null);
+		getContentPane().add(Logo);
+		getContentPane().add(FE7);
+		getContentPane().add(GBVS);
+
 	}
 
 }
